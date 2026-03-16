ea476fc99dee36ceb3bfe0dc14278bf8e57dfa67
Ranjan7210 <ranjan721.singh@gmail.com>
2026-03-16 23:22:39 +0530

    UC6

---
+++ OOPS_Banner.java
@@ -0,0 +1,47 @@
+public class OOPS_Banner {
+
+    public static String[] getOpattern() {
+        return new String[] {
+            "  ***  ",
+            " *   * ",
+            "*     *",
+            "*     *",
+            "*     *",
+            " *   * ",
+            "  ***  "
+        };
+    }
+
+    public static String[] getPpattern() {
+        return new String[] {
+            "****   ",
+            "*   *  ",
+            "*   *  ",
+            "****   ",
+            "*      ",
+            "*      ",
+            "*      "
+        };
+              }
+
+    public static String[] getSpattern() {
+        return new String[] {
+            " ***** ",
+            "*      ",
+            "*      ",
+            " ****  ",
+            "     * ",
+            "*    * ",
+            " ****  "
+        };
+    }
+    public static void main(String[] args) {
+
+        String[] oPattern = getOpattern();
+        String[] pPattern = getPpattern();
+        String[] sPattern = getSpattern();
+                for (int i = 0; i < oPattern.length; i++) {
+            System.out.println(oPattern[i] + " " + oPattern[i] + " " + pPattern[i] + " " + sPattern[i]);
+        }
+    }
+}
